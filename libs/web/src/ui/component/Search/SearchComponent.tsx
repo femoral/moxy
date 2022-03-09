f4f6a851c1f5da44929ce4a5ea66161dfc00b7aa
ggonzalezh <ggonzalezh15@gmail.com>
2022-03-09 02:21:17 -0300

    (:sparkles:) Added search collection

--- libs/web/src/ui/component/Search/SearchComponent.tsx
+++ libs/web/src/ui/component/Search/SearchComponent.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Input } from 'antd';
 
-export const SearchComponent = () => {
-  const onChange = (collectionName: string) => {
-    console.log(collectionName);
+export const SearchComponent = ({
+  onChange,
+  placeholder,
+}: SearchComponentProps) => {
+  const search = (name: string) => {
+    onChange(name);
   };
 
   return (
     <Input
       style={{ width: '40%' }}
-      placeholder="Search for collection"
+      placeholder={placeholder}
       size="middle"
-      onChange={(event) => onChange(event.target.value)}
+      onChange={(event) => search(event.target.value)}
     />
   );
 };
+
+interface SearchComponentProps {
+  placeholder: string;
+  onChange: (name: string) => void;
+}
