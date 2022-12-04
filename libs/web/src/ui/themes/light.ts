e62a49fa32621184fa9d70a9119c2ce6c18a7d32
ggonzalezh <ggonzalezh15@gmail.com>
2022-12-04 01:03:33 -0300

    :lipstick: Added initial file for theme antd V5

---
+++ libs/web/src/ui/themes/light.ts
@@ -0,0 +1,9 @@
+import { ThemeConfig } from 'antd/lib/config-provider/context';
+
+export const lightTheme: ThemeConfig = {
+  components: {
+    Layout: {
+      colorBgHeader: '#8BBCCC',
+    },
+  },
+};
