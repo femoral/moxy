84e33bd77020b512ec7adab032ab4e04ef4aff52
ggonzalezh <ggonzalezh15@gmail.com>
2022-12-04 01:54:33 -0300

    :lipstick: Testing color and components keys

--- libs/web/src/ui/themes/dark.ts
+++ libs/web/src/ui/themes/dark.ts
@@ -3,7 +3,23 @@ import { ThemeConfig } from 'antd/lib/config-provider/context';
 export const darkTheme: ThemeConfig = {
   components: {
     Layout: {
-      colorBgHeader: '#041C32',
+      colorBgHeader: '#18181B',
+      colorBgBody: '#0E0E10',
+    },
+    Menu: {
+      colorItemBg: '#1F1F23',
+      colorItemBgSelected: '#3C3C3F',
+
+      colorItemTextSelected: '#E5F1FD',
+      colorItemText: '#E5F1FD',
+
+      colorItemTextHover: '#E5F1FD',
+      colorItemBgHover: '#282828',
+
+      radiusItem: 0,
+    },
+    Card: {
+      colorBgContainer: '#3D3D40',
     },
   },
 };
